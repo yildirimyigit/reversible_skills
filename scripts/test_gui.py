7c22839ac5baaae0ecc79ac918bd6dd0521715b2
Yigit Yildirim <yigit.yildirim@bogazici.edu.tr>
2026-02-05 12:35:42 +0100

    feat: Accept any task with --task argument

--- scripts/test_gui.py
+++ scripts/test_gui.py
@@ -1,29 +1,38 @@
+import argparse
 import time
 from rlbench.environment import Environment
-from rlbench.tasks import PutRubbishInBin
+from rlbench import tasks
 from rlbench.observation_config import ObservationConfig
 from rlbench.action_modes.action_mode import MoveArmThenGripper
 from rlbench.action_modes.arm_action_modes import JointVelocity
 from rlbench.action_modes.gripper_action_modes import Discrete
 
-obs_config = ObservationConfig()
-obs_config.set_all(False)
-obs_config.front_camera.set_all(True)
 
-env = Environment(
-    MoveArmThenGripper(JointVelocity(), Discrete()),
-    obs_config=obs_config,
-    headless=False,   # show GUI
-)
-env.launch()
+def main():
+    ap = argparse.ArgumentParser()
+    ap.add_argument("--task", type=str, default="StackBlocks")
+    args = ap.parse_args()
 
-input("Environment launched. Press Enter to start demo...")
+    obs_config = ObservationConfig()
+    obs_config.set_all(False)
+    obs_config.front_camera.set_all(True)
 
-task = env.get_task(PutRubbishInBin)
-task.set_variation(0)
+    env = Environment(
+        MoveArmThenGripper(JointVelocity(), Discrete()),
+        obs_config=obs_config,
+        headless=False,   # show GUI
+    )
+    env.launch()
 
-print("Generating + running 1 live expert demo...")
-_ = task.get_demos(amount=1, live_demos=True)  # runs the demo in the sim
+    task = env.get_task(getattr(tasks, args.task))
+    task.set_variation(0)
 
-input("Demo finished. Press Enter to close...")
-env.shutdown()
+    input("Generating demo. Press Enter to start...")
+    _ = task.get_demos(amount=1, live_demos=True)  # runs the demo in the sim
+
+    input("Demo finished. Press Enter to close...")
+    env.shutdown()
+
+
+if __name__ == "__main__":
+    main()
